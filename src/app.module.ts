0a95acf4ca22a7fa51671842533a32f4315c01e8
yponion <yp071704@gmail.com>
2025-02-03 21:58:49 +0900

    feat: PostgreSQL 설치 및 세팅
    .forRoot를 .forRootAsync로 해서 환경변수 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from "./app.service";
 import { BoardModule } from "./board/board.module";
 import { LoggingMiddleware } from "./middleware/logging.middleware";
 import ConfigModule from "./config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [ConfigModule(), BoardModule],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [__dirname + "/**/*.entity.{ts,js}"],
+        synchronize: false,
+      }),
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
