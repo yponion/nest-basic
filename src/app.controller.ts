375332380bdf06c1a26c3adf692670d42b83e016
yponion <yp071704@gmail.com>
2025-02-03 20:00:59 +0900

    feat: LoggerModule 을 이용한 로깅 처리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Logger,
   Param,
   Query,
 } from "@nestjs/common";
@@ -13,9 +14,18 @@ import { Ip } from "./decorators/ip.decorator";
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  // private readonly logger = new Logger(AppController.name);
+
   @Get()
   getHello(@Ip() ip: string): string {
-    throw new HttpException("NotFound", HttpStatus.NOT_FOUND);
+    // this.logger.log(ip);
+    // this.logger.debug(ip);
+    // this.logger.error(ip);
+    // this.logger.verbose(ip);
+    // this.logger.warn(ip);
+
+    // throw new HttpException("NotFound", HttpStatus.NOT_FOUND);
+
     console.log(ip);
     return this.appService.getHello();
   }
