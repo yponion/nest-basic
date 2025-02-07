00f7e37aed9e3377d04d46df9d2000d22c72504c
양정운 <yp@yp-2.local>
2025-02-08 07:24:36 +0900

    feat: 게시판에 회원정보 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
 
   async login(user: User) {
     const payload = {
+      id: user.id,
       username: user.username,
       name: user.name,
     };
--- src/routes/board/board.controller.ts
+++ src/routes/board/board.controller.ts
@@ -7,12 +7,17 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Request,
+  UnauthorizedException,
+  UseGuards,
   ValidationPipe,
 } from "@nestjs/common";
 import { BoardService } from "./board.service";
 import { ApiTags } from "@nestjs/swagger";
 import { CreateBoardDto } from "./dto/create-board.dto";
 import { UpdateBoardDto } from "./dto/update-board.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserInfo } from "src/decorators/user-info.decorator";
 
 @Controller("board")
 @ApiTags("Board")
@@ -30,11 +35,21 @@ export class BoardController {
   }
 
   @Post()
-  create(@Body(new ValidationPipe()) data: CreateBoardDto) {
-    return this.boardService.create(data);
+  @UseGuards(JwtAuthGuard)
+  create(
+    @UserInfo() userInfo,
+    @Body() data: { title: string; content: string },
+  ) {
+    if (!userInfo) throw new UnauthorizedException();
+    return this.boardService.create({
+      userId: userInfo.id,
+      title: data.title,
+      content: data.content,
+    });
   }
 
   @Put(":id")
+  @UseGuards(JwtAuthGuard)
   update(
     @Param("id", ParseIntPipe) id: number,
     @Body(new ValidationPipe()) data: UpdateBoardDto,
@@ -43,6 +58,7 @@ export class BoardController {
   }
 
   @Delete(":id")
+  @UseGuards(JwtAuthGuard)
   remove(@Param("id", ParseIntPipe) id: number) {
     return this.boardService.delete(id);
   }
