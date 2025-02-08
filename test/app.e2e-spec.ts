bb50aaa9d005aa1194bc51107ad1cffb6c08c05e
양정운 <yp@yp-2.local>
2025-02-08 23:19:56 +0900

    feat: e2e 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("e2e 테스트", () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,37 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe("AppController", () => {
+    it("/ (GET)", () => {
+      return request(app.getHttpServer())
+        .get("/")
+        .expect(200)
+        .expect("Hello World!");
+    });
+
+    it("/name?name=yju (GET)", () => {
+      return request(app.getHttpServer())
+        .get("/name?name=yju")
+        .expect(200)
+        .expect("yju hello");
+    });
+
+    it("[로그인]", () => {
+      return request(app.getHttpServer())
+        .post("/login")
+        .send({
+          username: "user02ID",
+          password: "user02PW",
+        })
+        .expect(201);
+    });
   });
+
+  describe("BoardController", () => {
+    it("게시글 가져오기", () => {
+      return request(app.getHttpServer()).get("/board").expect(200);
+    });
+  });
+
+  describe("UserController", () => { });
 });
